d3af04dd36c2f4df4c8623518b4869bbf8303641
Radist <radist-electric@yandex.ru>
2020-12-05 02:04:38 +0500

    I created a table of ore characteristics

--- src/components/OreChar.js
+++ src/components/OreChar.js
@@ -1,6 +1,13 @@
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableContainer from '@material-ui/core/TableContainer'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import TableFooter from '@material-ui/core/TableFooter'
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -12,8 +19,50 @@ const useStyles = makeStyles(() => ({
   title: {
     fontWeight: 700
   },
+  tableHead: {
+    fontWeight: 400,
+    fontSize: '12px'
+  },
+  tableRow: {
+    height: '28px'
+  },
+  listButton: {
+    paddingBottom: '10px',
+    backgroundColor: '#ffffff',
+    fontSize: '16px',
+    fontWeight: 700,
+    border: 'none',
+    outline: 'none',
+    cursor: 'pointer'
+  },
 }))
 
+const characteristics = [
+  {
+    name: 'ПО',
+    data: ['63,97', '64,23']
+  },
+  {
+    name: 'Г',
+    data: ['20', '20']
+  },
+  {
+    name: 'Ю',
+    data: ['10', '15']
+  },
+  {
+    name: 'З',
+    data: ['25', '35']
+  },
+  {
+    name: 'С',
+    data: ['40', '25']
+  },
+  {
+    name: 'Э 48',
+    data: ['5', '5']
+  },
+]
 
 
 export default function OreChar() {
@@ -26,6 +75,34 @@ export default function OreChar() {
           <Typography component="h2" className={classes.title}>
             Характеристики руды, %
           </Typography>
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell></TableCell>
+                  <TableCell className={classes.tableHead}>Текущее</TableCell>
+                  <TableCell className={classes.tableHead}>Через 1 час</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {characteristics.map((row, i) => (
+                  <TableRow key={i} className={classes.tableRow}>
+                    <TableCell>{row.name}</TableCell>
+                    {row.data.map((elem, i) => (
+                      <TableCell key={i}>{elem}</TableCell>
+                    ))}
+                  </TableRow>
+                ))}
+              </TableBody>
+              <TableFooter>
+                <TableRow>
+                  <TableCell><button className={classes.listButton}>...</button></TableCell>
+                  <TableCell>-</TableCell>
+                  <TableCell>-</TableCell>
+                </TableRow>
+              </TableFooter>
+            </Table>
+          </TableContainer>
         </Grid>
       </Grid>
     </div>
